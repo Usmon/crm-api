ac8309c560936b45785cc25d3702e6ea925940c9
iAxel <iaxel0297@gmail.com>
2020-11-12 12:01:58 +0500

    Update Auth.php file

--- app/Models/Foundation/Auth.php
+++ app/Models/Foundation/Auth.php
@@ -8,12 +8,14 @@ use App\Models\Foundation\Traits\VerifyToken;
 use App\Models\Foundation\Traits\Authorizable;
 use App\Models\Foundation\Traits\Authenticatable;
 
+use App\Models\Foundation\Contracts\ResetToken as ResetTokenContract;
+use App\Models\Foundation\Contracts\VerifyToken as VerifyTokenContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 use Illuminate\Database\Eloquent\Model;
 
-abstract class Auth extends Model implements AuthenticatableContract, AuthorizableContract
+abstract class Auth extends Model implements AuthenticatableContract, AuthorizableContract, ResetTokenContract, VerifyTokenContract
 {
     use Authenticatable;
     use Authorizable;
