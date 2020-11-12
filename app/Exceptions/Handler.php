4d1da1bc21dddea1a90fece4357cb641acd3625f
iAxel <iaxel0297@gmail.com>
2020-11-12 11:32:30 +0500

    Refactoring Exceptions Handler.php file

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,30 +2,34 @@
 
 namespace App\Exceptions;
 
+use Throwable;
+
+use App\Helper\Json;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as Exceptions;
+
+use Symfony\Component\HttpFoundation\Response;
 
 final class Handler extends Exceptions
 {
     /**
-     * @var array
+     * @param Request $request
+     * @param Throwable $e
+     *
+     * @return Response
+     *
+     * @throws Throwable
      */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * @return void
-     */
-    public function register(): void
+    public function render($request, Throwable $e)
     {
-        //
+        if ($e instanceof ValidationException) {
+            return Json::sendResponseWith422([
+                'errors' => $e->errors(),
+            ]);
+        }
+
+        return parent::render($request, $e);
     }
 }
