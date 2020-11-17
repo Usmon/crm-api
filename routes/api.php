f45b832029ad704f23b41ecd0eecff7b75584953
iAxel <iaxel0297@gmail.com>
2020-11-17 22:24:29 +0500

    Update routes api.php file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Helpers\Json;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -8,13 +10,15 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-use App\Http\Controllers\App as AppController;
-
 use App\Http\Controllers\Auth\Login as AuthLoginController;
 
 use App\Http\Controllers\Auth\Logout as AuthLogoutController;
 
 use App\Http\Controllers\Auth\Register as AuthRegisterController;
+
+use App\Http\Controllers\User\Controller as UserController;
+
+use App\Http\Controllers\User\Sessions\Controller as UserSessionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,11 +27,34 @@ use App\Http\Controllers\Auth\Register as AuthRegisterController;
 */
 
 // App routers
-Route::get('/', AppController::class)->name('app');
+Route::name('app')->get('/', function () {
+    return Json::sendJsonWith200([
+        'app' => [
+            'name' => config('app.name'),
+
+            'version' => config('app.version'),
+        ],
+    ]);
+});
 
 // Authentication routes
 Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
     Route::post('login', AuthLoginController::class)->name('login');
+
     Route::post('logout', AuthLogoutController::class)->name('logout');
+
     Route::post('register', AuthRegisterController::class)->name('register');
+});
+
+// User routes
+Route::group(['prefix' => 'user', 'middleware' => 'auth:api'], function () {
+    Route::get('/', UserController::class)->name('user');
+
+    Route::group(['prefix' => 'sessions', 'as' => 'sessions.'], function () {
+        Route::get('/', [UserSessionsController::class, 'index'])->name('index');
+
+        Route::delete('other', [UserSessionsController::class, 'other'])->name('other');
+
+        Route::delete('{token}', [UserSessionsController::class, 'delete'])->name('delete');
+    });
 });
