ec3e1a2482fbaee440ff16957d733bce74ccd379
iAxel <iaxel0297@gmail.com>
2020-11-17 22:25:29 +0500

    Add User Sessions Controller

---
+++ app/Http/Controllers/User/Sessions/Controller.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\User\Sessions;
+
+use App\Helpers\Json;
+
+use App\Models\Token;
+
+use App\Http\Controllers\Controller as Controllers;
+
+use App\Logic\User\Services\Sessions as SessionsService;
+
+use App\Logic\User\Repositories\Sessions as SessionsRepository;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Http\JsonResponse;
+
+class Controller extends Controllers
+{
+    /**
+     * @var SessionsService
+     */
+    protected $service;
+
+    /**
+     * @var SessionsRepository
+     */
+    protected $repository;
+
+    /**
+     * @param SessionsService $service
+     *
+     * @param SessionsRepository $repository
+     *
+     * @return void
+     */
+    public function __construct(SessionsService $service, SessionsRepository $repository)
+    {
+        $this->service = $service;
+
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        return Json::sendJsonWith200([
+            'sessions' => $this->service->getAll($this->repository->all($this->service->getUser($request)), $this->service->getBearerToken($request)),
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function other(Request $request): JsonResponse
+    {
+        $this->repository->other($this->service->getUser($request), $this->service->getBearerToken($request));
+
+        return Json::sendJsonWith200([
+            'message' => 'The other sessions successfully deleted.',
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @param Token $token
+     *
+     * @return JsonResponse
+     */
+    public function delete(Request $request, Token $token): JsonResponse
+    {
+        if (! $this->repository->delete($this->service->getUser($request), $token)) {
+            return Json::sendJsonWith409([
+                'message' => 'Failed to delete session, please try again later.',
+            ]);
+        }
+
+        return Json::sendJsonWith200([
+            'message' => 'The session successfully deleted.',
+        ]);
+    }
+}
