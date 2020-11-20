6afb4793fda997580553c43be5ec47f1a8aac8e9
My Name <murodbek99.murodov@gmail.com>
2020-11-20 15:20:18 +0500

    added service and repo for Roles

---
+++ app/Logic/Dashboard/CRUD/Services/Roles.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Logic\Dashboard\CRUD\Services;
+
+use App\Models\Role;
+
+use App\Logic\Dashboard\CRUD\Requests\Roles as RolesRequest;
+
+use Illuminate\Support\Arr;
+
+use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Database\Eloquent\Collection;
+
+final class Roles
+{
+    /**
+     * @param RolesRequest $request
+     *
+     * @return array
+     */
+    public function getAllFilters(RolesRequest $request): array
+    {
+        return [
+            'search' => $request->json('search'),
+
+            'date' => $request->json('date'),
+
+            'permissions' => $request->json('permissions'),
+        ];
+    }
+
+    /**
+     * @param RolesRequest $request
+     *
+     * @return array
+     */
+    public function getOnlyFilters(RolesRequest $request): array
+    {
+        return $request->only('search', 'date', 'permissions');
+    }
+
+    /**
+     * @param Collection $collection
+     *
+     * @return Collection
+     */
+    public function getRoles(Collection $collection): Collection
+    {
+        return $collection->transform(function (Roles $role) {
+            return [
+                'id' => $role->id,
+
+                'name' => $role->name,
+
+                'slug' => $role->slug,
+
+                'description' => $role->description,
+
+                'created_at' => $role->created_at,
+
+                'updated_at' => $role->updated_at,
+            ];
+        });
+    }
+
+    /**
+     * @param Role $role
+     *
+     * @return array
+     */
+    public function showRole(Role $role): array
+    {
+        return [
+            'id' => $role->id,
+
+            'name' => $role->name,
+
+            'slug' => $role->slug,
+
+            'description' => $role->description,
+
+            'created_at' => $role->created_at,
+
+            'updated_at' => $role->updated_at,
+        ];
+    }
+
+    /**
+     * @param RolesRequest $request
+     *
+     * @return array
+     */
+    public function storeCredentials(RolesRequest $request): array
+    {
+        return [
+            'name' => $request->json('name'),
+
+            'slug' => $request->json('slug'),
+
+            'description' => $request->json('description'),
+
+            'permissions' => $request->json('permissions'),
+        ];
+    }
+
+    /**
+     * @param RolesRequest $request
+     *
+     * @return array
+     */
+    public function updateCredentials(RolesRequest $request): array
+    {
+        $credentials = [
+            'name' => $request->json('name'),
+
+            'slug' => $request->json('slug'),
+
+            'description' => $request->json('description'),
+
+            'permissions' => $request->json('permissions'),
+        ];
+
+        return $credentials;
+    }
+
+
+}
