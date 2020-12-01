245918367071114e0053eea9b2b067e880a7d4ee
Usmon <usmonzaripov@gmail.com>
2020-12-01 15:42:34 +0500

    Pager construct remove and config

--- app/Traits/Pagination/Pager.php
+++ app/Traits/Pagination/Pager.php
@@ -12,22 +12,6 @@ use Illuminate\Database\Eloquent\Builder;
 trait Pager
 {
     /**
-     * Size of page
-     * 
-     * @property int
-     */
-    private $page_size;
-    
-    /**
-     * Construct for Pagination trait
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->page_size = env('PAGE_SIZE', 20);
-    }
-
-    /**
      * Pager method
      * 
      * @param int $size
@@ -36,6 +20,6 @@ trait Pager
      */
     public function scopePager(Builder $query,int $size = 0)
     {
-        return $query->paginate($size > 0 ? $size : $this->page_size);
+        return $query->paginate($size > 0 ? $size : config('app.page_size'));
     }
 }
