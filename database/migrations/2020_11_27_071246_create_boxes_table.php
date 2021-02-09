5c6d9d682536cfe70f496f92c695b14909a08562
Usmon <usmonzaripov@gmail.com>
2021-02-09 15:18:30 +0500

    Migration modify for Orders part 2

---
+++ database/migrations/2020_11_27_071246_create_boxes_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+
+use Illuminate\Database\Schema\Blueprint;
+
+use Illuminate\Database\Migrations\Migration;
+
+final class CreateBoxesTable extends Migration
+{
+    /**
+     * @var string
+     */
+    protected $table = 'boxes';
+
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('order_id');
+
+            $table->float('weight');
+
+            $table->float('additional_weight');
+
+            $table->string('status');
+
+            $table->string('box_image');
+
+            $table->timestamp('created_at')->nullable();
+
+            $table->timestamp('updated_at')->nullable();
+
+            $table->timestamp('deleted_at')->nullable();
+        });
+
+        Schema::table($this->table, function (Blueprint $table) {
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            
+        });
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
