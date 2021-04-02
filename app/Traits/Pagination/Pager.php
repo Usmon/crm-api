314d47354b0c75d4bcf4e8439faa3cccb780e1b5
Usmon <usmonzaripov@gmail.com>
2021-04-02 09:16:48 +0500

    Pager PHP docs fixes

--- app/Traits/Pagination/Pager.php
+++ app/Traits/Pagination/Pager.php
@@ -4,21 +4,25 @@ namespace App\Traits\Pagination;
 
 use Illuminate\Database\Eloquent\Builder;
 
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
 /**
  * Trait pager for pagination collections
- * 
+ *
  * @method static Illuminate\Pagination\Paginator pager(int $size)
  */
 trait Pager
 {
     /**
      * Pager method
-     * 
+     *
+     * @param Builder $query
+     *
      * @param int $size
-     * 
-     * @return \Illuminate\Pagination\Paginator
+     *
+     * @return LengthAwarePaginator
      */
-    public function scopePager(Builder $query,int $size = 0)
+    public function scopePager(Builder $query,int $size = 0): LengthAwarePaginator
     {
         return $query->paginate(request()->get('page_size') ?? $size ?? config('app.page_size'));
     }
