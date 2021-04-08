09faedcd2a101afd7a4daa653b0ad307fad2a3e8
Usmon <usmonzaripov@gmail.com>
2021-04-08 20:34:32 +0500

    Customer limit check rules, logic ready

---
+++ app/Http/Controllers/Dashboard/Orders/Limit/Controller.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard\Orders\Limit;
+
+use App\Helpers\Json;
+
+use Illuminate\Http\JsonResponse;
+
+use App\Logic\Dashboard\CRUD\Requests\Limit as LimitRequest;
+
+use App\Http\Controllers\Controller as Controllers;
+
+/**
+ * Class Controller
+ *
+ * @package App\Http\Controllers\Dashboard\Orders\Limit
+ */
+class Controller extends Controllers
+{
+
+    /**
+     * @param LimitRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function checkSender(LimitRequest $request): JsonResponse
+    {
+        return Json::sendJsonWith200([
+            'message' => 'Check all nice!'
+        ]);
+    }
+}
---
+++ app/Rules/CustomerLimit.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Rules;
+
+use Carbon\Carbon;
+
+use App\Models\Order;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * Class CustomerLimit
+ *
+ * @package App\Rules
+ */
+final class CustomerLimit implements Rule
+{
+    /**
+     * @const integer
+     */
+    const LIMIT = 1000;
+
+    /**
+     * @var array
+     */
+    private $quarter = [];
+
+    /**
+     * @return void
+     */
+    public function __construct()
+    {
+        $carbon = new Carbon;
+
+        $this->quarter = [
+            $carbon->firstOfQuarter()->toDateString(),
+
+            $carbon->lastOfQuarter()->toDateString()
+        ];
+    }
+
+    /**
+     * @param string $attribute
+     *
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        return $this->checker($attribute, $value);
+    }
+
+    /**
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'The :attribute limit out side.';
+    }
+
+    public function checker($attribute, $value)
+    {
+        return Order::where($attribute, $value)
+                    ->whereBetween('created_at', $this->quarter)
+                    ->sum('price_total')
+                    < self::LIMIT;
+    }
+}
