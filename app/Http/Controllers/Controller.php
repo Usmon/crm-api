09ce246ac2b7ca4b8a9d583f2c3e3f9fbd4ddabf
Usmon <usmonzaripov@gmail.com>
2021-06-01 11:20:37 +0500

    Roles checker optimize

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\Json;
+
+use Illuminate\Http\JsonResponse;
+
+use Illuminate\Support\Facades\Gate;
+
 use Illuminate\Routing\Controller as Routing;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
@@ -15,4 +21,18 @@ abstract class Controller extends Routing
     use AuthorizesRequests;
     use ValidatesRequests;
     use DispatchesJobs;
+
+    /**
+     * @param string $permission
+     *
+     * @return mixed
+     * */
+    protected function checkPermission(string $permission)
+    {
+        if(! Gate::check($permission)){
+            return Json::sendJsonWith403([
+                'message' =>  'Permission denied.'
+            ]);
+        }
+    }
 }
