7fc6ff113e906bb5694608592c5bf807f0c00e85
Usmon <usmonzaripov@gmail.com>
2021-06-11 09:25:52 +0500

    Add full name to users list

--- app/Logic/Dashboard/CRUD/Services/Users.php
+++ app/Logic/Dashboard/CRUD/Services/Users.php
@@ -51,6 +51,8 @@ final class Users
             return [
                 'id' => $user->id,
 
+                'full_name' => $user->full_name,
+
                 'login' => $user->login,
 
                 'email' => $user->email,
